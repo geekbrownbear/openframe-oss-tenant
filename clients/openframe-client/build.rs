531c810f9428a90d9014bda299f1cd6f4e680205
kirill-567 <kirill@flamingo.cx>
2026-03-25 19:28:36 +0100

    Revert "Initial client version tracking  " (#1634)
    Co-authored-by: Kirill Gontar <Kirill Gontar>

--- clients/openframe-client/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-env-changed=OPENFRAME_VERSION");
-
-    let version = std::env::var("OPENFRAME_VERSION")
-        .expect("OPENFRAME_VERSION environment variable must be set at build time");
-    println!("cargo:rustc-env=OPENFRAME_VERSION={}", version);
-}
